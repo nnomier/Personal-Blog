8f828a53766ff3a4d5cdecc168e85e98a6ddd488
noha nomier <nnomier@gmail.com>
2019-08-25 17:52:12 -0700

    Add post pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -25,7 +26,6 @@ app.get("/", function(req, res) {
     content: homeStartingContent,
     postsArray: posts
   });
-  console.log(posts[0].post);
 });
 
 app.get("/about", function(req, res) {
@@ -45,7 +45,17 @@ app.get("/contact", function(req, res) {
 
 app.get("/compose", function(req, res) {
   res.render("compose");
+});
 
+app.get('/posts/:postName', function(req, res) {
+  posts.forEach(function(Post) {
+    if (_.lowerCase(req.params.postName) === _.lowerCase(Post.title)) {
+      res.render("post", {
+        postTitle: Post.title,
+        postContent: Post.post
+      });
+    }
+  });
 });
 
 app.post("/compose", function(req, res) {
