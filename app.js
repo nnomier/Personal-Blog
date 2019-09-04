9981b529d863651d00160e98fa27e1b35ccc7b08
noha nomier <nnomier@gmail.com>
2019-09-04 15:21:13 -0700

    add new post feature and more style

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.connect("mongodb+srv://admin-noha:TesT123@cluster0-v01vq.mongodb.net/bl
 
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to my blog, feel free to add anything that comes to you mind whether it's a personal story, a fun fact or just a random thought";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -38,7 +38,7 @@ app.get("/", function(req, res) {
 
   Post.find({}, function(err, posts) {
     res.render("home", {
-      pageTitle:"Home",
+      pageTitle:"Daily Journal",
       content: homeStartingContent,
       posts: posts
     });
@@ -48,10 +48,7 @@ app.get("/", function(req, res) {
 
 
 app.get("/about", function(req, res) {
-  res.render("about", {
-    pageTitle: "About",
-    content: aboutContent
-  });
+  res.render("about");
 });
 
 app.get("/contact", function(req, res) {
